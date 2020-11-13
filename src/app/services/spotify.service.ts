556d05c62f99cf0b77646611a573e3daf797b811
Janio Isacura <janioisacura@gmail.com>
2020-11-13 19:44:08 -0300

    Se agregó lógica de búsqueda

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,9 +13,17 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBsc-K50YS-RvjTduIn_lcv-tqCZTDOwNAw-36GHWWp4SxHXyfwqkOgt4v1zzz6HYTGUWQaao-VjEIaEHKj8qtKLpBVeriFjq8RfyrJ7IbFOo0PACQ7eAO-RPdj_wMfQxl9mgjLPqhqo7tWvQ'
+      'Authorization': 'Bearer BQBHdVPpeXVeAHqdujppbPvJs_4Uy9FswnIz4DMSPUhI3obnA7Eb0xD235aqkTd9hC5E94zvHd5-w3IZW-A'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers });  
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers });
+  }
+  getArtista( termino:string ) {
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBHdVPpeXVeAHqdujppbPvJs_4Uy9FswnIz4DMSPUhI3obnA7Eb0xD235aqkTd9hC5E94zvHd5-w3IZW-A'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 }
