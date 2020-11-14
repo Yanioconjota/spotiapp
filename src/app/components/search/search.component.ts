df43f790fc47dea548006eb8fa1eea486c5db5c2
Janio Isacura <janioisacura@gmail.com>
2020-11-13 21:34:01 -0300

    Optimización servicio spotify

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -17,7 +17,7 @@ export class SearchComponent {
     console.log(termino);
     this.spotify.getArtista( termino )
     .subscribe((data: any) => {
-        this.artistas = data.artists.items;
+        this.artistas = data;
         console.log(data);
       });
   }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
+  token: string = 'Bearer BQCkP8TdNsDQKwqLxH_YuVagnWTCfHcOrgT1WD42y8n8PfEHrVwGfdYKh-RiNXebLI7M7Z3wsbpQrpnfBr8';
 
   constructor(private http: HttpClient) {
     console.log('spotify service listo');
@@ -13,17 +15,19 @@ export class SpotifyService {
   getNewReleases() {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBHdVPpeXVeAHqdujppbPvJs_4Uy9FswnIz4DMSPUhI3obnA7Eb0xD235aqkTd9hC5E94zvHd5-w3IZW-A'
+      'Authorization': `${this.token}`
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20',{ headers })
+      .pipe(map(data => data['albums'].items));
   }
   getArtista( termino:string ) {
 
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQBHdVPpeXVeAHqdujppbPvJs_4Uy9FswnIz4DMSPUhI3obnA7Eb0xD235aqkTd9hC5E94zvHd5-w3IZW-A'
+      'Authorization': `${this.token}`
     });
 
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
+      .pipe(map((data) => data['artists'].items));
   }
 }
